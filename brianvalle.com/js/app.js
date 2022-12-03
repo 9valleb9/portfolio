7288b88f3bbf2d6915a57163d75011cc2713e266
Brian Valle <email@brianvalle.com>
2022-12-02 22:31:37 -0800

    Updated Portfolio Site

--- brianvalle.com/js/app.js
+++ brianvalle.com/js/app.js
@@ -1,83 +1,71 @@
-/*===========GOOGLE FONTS==============*/
-
-
-/*===========vARIABLE CSS==============*/
-
-
-/*===========COLORS==============*/
-
-/*===========FONTS==============*/
-
-
-/*===========MARGINS==============*/
-
-
-/*===========Z INDEX==============*/
-
-
-/*===========BASE==============*/
-
-
-/*===========CLASS CSS==============*/
-
-
-/*===========LAYOUT==============*/
-
-
-/*header color and layout*/
-
-/*===========NAV MENU==============*/
-
-
-/*===========SHOW MENU==============*/
-
-const showMenu = (toggleId, navId) => {
+/*===== MENU SHOW =====*/ 
+const showMenu = (toggleId, navId) =>{
     const toggle = document.getElementById(toggleId),
     nav = document.getElementById(navId)
 
-    if(toggle && nav) {
-        toggle.addEventListener('click', () => {
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
             nav.classList.toggle('show')
         })
     }
 }
-showMenu ('nav-toggle','nav-menu')
+showMenu('nav-toggle','nav-menu')
 
-/*===========Remove Mobile MENU==============*/
-
+/*===== REMOVE MENU MOBILE =====*/
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction(){
     const navMenu = document.getElementById('nav-menu')
     navMenu.classList.remove('show')
-
 }
-
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-
-/*===========Scroll Sections active link==============*/
-
+/*===== SCROLL SECTIONS ACTIVE LINK =====*/
 const sections = document.querySelectorAll('section[id]')
 
-window.addEventLinster('scroll' scrollActive)
+window.addEventListener('scroll', scrollActive)
 
-function scrollActive{
-    const scrollY = window.pageYoffset
+function scrollActive(){
+    const scrollY = window.pageYOffset
 
-    sections.forEach(current => {
+    sections.forEach(current =>{
         const sectionHeight = current.offsetHeight
-        const sectionTop = current.offsetHeight - 50;
+        const sectionTop = current.offsetTop - 50;
         sectionId = current.getAttribute('id')
 
-
-        if(scrollY > sectionTop && scrollY < sectionTop + sectionHeight){
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
         }else{
             document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
         }
-
     })
 }
 
-/*===========ACTIVE MENU==============*/
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+/*SCROLL HOME*/
+sr.reveal('.home__title', {})
+sr.reveal('.home__scroll', {delay: 200})
+sr.reveal('.home__img', {origin:'right', delay: 400})
+
+/*SCROLL ABOUT*/
+sr.reveal('.about__img', {delay: 500})
+sr.reveal('.about__subtitle', {delay: 300})
+sr.reveal('.about__profession', {delay: 400})
+sr.reveal('.about__text', {delay: 500})
+sr.reveal('.about__social-icon', {delay: 600, interval: 200})
+
+/*SCROLL SKILLS*/
+sr.reveal('.skills__subtitle', {})
+sr.reveal('.skills__name', {distance: '20px', delay: 50, interval: 100})
+sr.reveal('.skills__img', {delay: 400})
+
+/*SCROLL PORTFOLIO*/
+sr.reveal('.portfolio__img', {interval: 200})
+
